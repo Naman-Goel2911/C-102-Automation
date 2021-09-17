287abb622d22d5d2a5b083b89f94db67af22ad60
Naman-Goel2911 <73833786+Naman-Goel2911@users.noreply.github.com>
2021-09-17 15:14:27 +0530

    Add files via upload

--- tempFilesDeleter.py
+++ tempFilesDeleter.py
@@ -1,24 +1,48 @@
 import shutil
 import os
+import time
 
 def tempFilesDelete():
+    deletedFoldersCount = 0
+    deletedFileCount = 0
+    days = 7
+    seconds = time.time() - (days*24*60*60)
     path = input('Enter the full path to the temp folder: ')
 
-    if(os.path.exists(path)):
-        for folders, files in os.walk(path):
+    if os.path.exists(path):
+        for rootFolder, folders, files in os.walk(path):
+            if seconds>= getFileOrFolderAge(rootFolder):
+                removeFolder(rootFolder)
+                deletedFoldersCount += 1
+                break
+            else:
+                for folder in folders:
+                    folderPath = os.path.join(rootFolder, folder)
+                    if seconds >= getFileOrFolderAge(folderPath):
+                        removeFolder(folderPath)
+                        deletedFoldersCount += 1
 
-            for folder in folders:
-                folderPath = os.path.join(path, folder)
-                removeFolder(folderPath)
+                for file in files:
+                    filePath = os.path.join(rootFolder, file)
+                    if seconds >= getFileOrFolderAge(filePath):
+                        removeFile(filePath)
+                        deletedFileCount += 1
 
-            for file in files:
-                filePath = os.path.join(path, file)
-                name, ext = os.path.splitext(file)
-                ext = ext[1:]
-                if(ext == 'temp'):
-                    removeFile(filePath)    
-    else:
-        print('Oops, the path is wrong.')
+        else:
+            if seconds>= getFileOrFolderAge(path):
+                removeFile(path)
+                deletedFileCount += 1   
+
+    else: 
+        print(f'{path} is not found.')
+        deletedFileCount += 1
+        
+    print(f'Total Folders deleted: {deletedFoldersCount}')
+    print(f'Total Files deleted: {deletedFileCount}')
+
+def getFileOrFolderAge(path):
+    ctime = os.stat(path).st_ctime
+    return ctime
 
 def removeFolder(path):
     if not shutil.rmtree(path):
